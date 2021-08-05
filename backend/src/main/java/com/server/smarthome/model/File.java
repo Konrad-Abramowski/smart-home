10d0383eeb1ef72ada76f97aa4db3afa218f2306
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-08-05 20:04:13 +0200

    Add model classes for print server #7

---
+++ backend/src/main/java/com/server/smarthome/model/File.java
@@ -0,0 +1,58 @@
+package com.server.smarthome.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
+public class File {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    private byte[] data;
+
+    public File() {}
+
+    public File(final String name, final String type, final byte[] data) {
+        this.name = name;
+        this.type = type;
+        this.data = data;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setType(final String type) {
+        this.type = type;
+    }
+
+    public void setData(final byte[] data) {
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+}
