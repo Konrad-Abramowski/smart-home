1b92d1bf5c0fec940770dccdb2a819491e19feb4
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-08-11 20:36:24 +0200

    Update app module #2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
-import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHeaderComponent } from './header/app-header.component';
+import { AppFooterComponent } from './footer/app-footer.component';
 
 @NgModule({
   declarations: [
