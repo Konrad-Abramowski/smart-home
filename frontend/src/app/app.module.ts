3973b47b18a309c27de3ca37afe064efe7e2119d
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-08-11 22:07:46 +0200

    Fix bootstrap #12

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './header/app-header.component';
-import { AppFooterComponent } from './footer/app-footer.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent,
-    AppFooterComponent,
-    FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
