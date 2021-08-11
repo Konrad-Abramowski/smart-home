da9425133d901e172e33454fba7584d467e58dae
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-08-11 20:37:45 +0200

    Rename all component files #2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './header/app-header.component';
 import { AppFooterComponent } from './footer/app-footer.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
