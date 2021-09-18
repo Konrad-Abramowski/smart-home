6d4917e5ab436364eca9d0235d0dfb096e0f5b52
konrad-abramowski <abramowski.konrad@gmail.com>
2021-09-19 00:03:17 +0200

    Remove footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {MainComponent} from "./components/main/main.component";
@@ -15,7 +14,6 @@ import { FileCardComponent } from './components/main/file-card/file-card.compone
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     MainComponent,
     FileCardComponent
   ],
