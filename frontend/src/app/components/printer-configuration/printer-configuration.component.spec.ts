864b3ac9620db5bf440f26257caddd18b81e23bf
konrad-abramowski <abramowski.konrad@gmail.com>
2021-09-19 00:29:11 +0200

    Create printer configuration component

---
+++ frontend/src/app/components/printer-configuration/printer-configuration.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrinterConfigurationComponent } from './printer-configuration.component';
+
+describe('PrinterConfigurationComponent', () => {
+  let component: PrinterConfigurationComponent;
+  let fixture: ComponentFixture<PrinterConfigurationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrinterConfigurationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrinterConfigurationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/printer-configuration/printer-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-printer-configuration',
+  templateUrl: './printer-configuration.component.html',
+  styleUrls: ['./printer-configuration.component.scss']
+})
+export class PrinterConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
