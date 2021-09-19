cb368aa8cc57e749742c20696ab3c7a8fbf4b2a4
konrad-abramowski <abramowski.konrad@gmail.com>
2021-09-19 22:15:50 +0200

    Small refactor #30

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PrinterConfigurationComponent} from "./components/printer-configuration/printer-configuration.component";
-import {MainComponent} from "./components/main/main.component";
+import {PrinterMainComponent} from "./components/printer-main/printer-main.component";
 
 const routes: Routes = [
-  {path: '', component: MainComponent},
+  {path: '', component: PrinterMainComponent},
   {path: 'configuration/printer', component: PrinterConfigurationComponent}
 ];
 
--- frontend/src/app/components/printer-main/printer-main.component.ts
+++ frontend/src/app/components/printer-main/printer-main.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FileService} from "../../services/file-service/file.service";
 
 @Component({
   selector: 'app-printer-main',
   templateUrl: './printer-main.component.html',
-  styleUrls: ['./printer-main.component.scss']
+  styleUrls: ['./printer-main.component.scss'],
+  providers: [FileService]
 })
 export class PrinterMainComponent implements OnInit {
 
-  constructor() { }
+  files: File[] = [];
+  filesToAdd: File[] = []
+
+  constructor(private fileService: FileService) {
+  }
 
   ngOnInit(): void {
+    this.updateFiles()
+  }
+
+  uploadFiles(fileList: FileList) {
+    this.fileService.uploadFiles(Array.prototype.slice.call(fileList))
+    this.updateFiles()
+  }
+
+  updateFiles() {
+    this.fileService.getFiles().subscribe(files => {
+      this.files = files
+    })
+  }
+
+  updateFilesToUpload(fileList: FileList) {
+    this.filesToAdd = Array.from(fileList)
   }
 
 }
