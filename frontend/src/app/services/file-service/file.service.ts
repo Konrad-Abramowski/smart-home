aae3409382d497bfe9f4eed4198f7c12eeaae349
konrad-abramowski <abramowski.konrad@gmail.com>
2021-11-06 02:13:27 +0100

    Ticket #22

--- frontend/src/app/services/file-service/file.service.ts
+++ frontend/src/app/services/file-service/file.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {PrinterConfiguration} from "../../model/printer-configuration";
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +12,35 @@ export class FileService {
   constructor(private httpClient: HttpClient) {
   }
 
-  uploadFiles(files: File[]) {
+  uploadFiles(files: File[]): Observable<File[]> {
     const formData = new FormData();
     files.forEach(file => {
       formData.append("file", file);
     })
-    const upload$ = this.httpClient.post("http://localhost:8080/upload", formData);
-    upload$.subscribe();
+    return this.httpClient.post<File[]>("http://localhost:8080/upload", formData)
   }
 
   getFiles(): Observable<File[]> {
     return this.httpClient.get<File[]>("http://localhost:8080/files")
   }
 
-  deleteFile(id: string) {
-    this.httpClient.delete("http://localhost:8080/files/" + id).subscribe()
+  deleteFile(id: string): Observable<File[]> {
+    return this.httpClient.delete<File[]>("http://localhost:8080/files/" + id)
+  }
+
+  printAll() {
+    this.httpClient.post("http://localhost:8080/printAll", null).subscribe()
+  }
+
+  getAvailablePrinters(): Observable<string[]> {
+    return this.httpClient.get<string[]>("http://localhost:8080/printer/available-printers")
+  }
+
+  setPrinter(printerToSet: string) {
+    this.httpClient.post("http://localhost:8080/printer/config/" + printerToSet, null).subscribe()
+  }
+
+  getPrinter(): Observable<PrinterConfiguration> {
+    return this.httpClient.get<PrinterConfiguration>("http://localhost:8080/printer/config/")
   }
 }
