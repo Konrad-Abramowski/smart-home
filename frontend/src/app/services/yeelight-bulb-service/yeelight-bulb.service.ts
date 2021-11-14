aaf9dcd0b78c722863ebc92406876607977bb1d8
konrad-abramowski <abramowski.konrad@gmail.com>
2021-11-14 13:54:26 +0100

    Ticket #47

--- frontend/src/app/services/yeelight-bulb-service/yeelight-bulb.service.ts
+++ frontend/src/app/services/yeelight-bulb-service/yeelight-bulb.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {YeelightBulb} from "../../model/yeelight-bulb";
+import {rgb} from "../../model/rgb";
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,16 @@ export class YeelightBulbService {
   }
 
   toggle(ip: string) {
-    return this.httpClient.get("http://localhost:5000/" + ip + "/toggle")
+    return this.httpClient.get("http://localhost:5000/" + ip + "/toggle").subscribe()
+  }
+
+  setColor(ip: string, value: rgb) {
+    this.httpClient.get("http://localhost:5000/" + ip + "/rgb", {
+      params: {
+        red: value.red,
+        green: value.green,
+        blue: value.blue,
+      }
+    }).subscribe()
   }
 }
