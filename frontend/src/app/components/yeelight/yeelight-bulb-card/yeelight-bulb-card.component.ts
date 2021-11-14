ae18b80a0f300b07be1a592a204c403741fdc49e
konrad-abramowski <abramowski.konrad@gmail.com>
2021-11-14 01:42:23 +0100

    Divide yeelight main component

---
+++ frontend/src/app/components/yeelight/yeelight-bulb-card/yeelight-bulb-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yeelight-bulb-card',
+  templateUrl: './yeelight-bulb-card.component.html',
+  styleUrls: ['./yeelight-bulb-card.component.scss']
+})
+export class YeelightBulbCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
