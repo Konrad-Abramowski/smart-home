a96de6f94a6e046fe1e4ffadbbebd8d58c11ab17
Konrad Abramowski <abramowski.konrad@gmail.com>
2021-11-21 23:20:52 +0100

    Fix IPs for development host

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  yeelightApiUrl: 'http://192.168.0.59:5000',
-  fileApiUrl: 'http://192.168.0.59:8080'
+  yeelightApiUrl: 'http://192.168.0.115:5000',
+  fileApiUrl: 'http://192.168.0.115:8080'
 };
